fcce32b68d6d5d7d3b0a76f21ec0bf59d92313c1
Louissmnnt <123833966+Louissmnnt@users.noreply.github.com>
2024-04-25 21:19:14 +0200

    Add files via upload

--- Final_assignment/ipsa_sched.c
+++ Final_assignment/ipsa_sched.c
@@ -94,20 +94,21 @@
 #define SIZE 50
 
 /* Priorities at which the tasks are created. */
-#define mainQUEUE_RECEIVE_TASK_PRIORITY     ( tskIDLE_PRIORITY + 3 )
+#define mainQUEUE_RECEIVE_TASK_PRIORITY     ( tskIDLE_PRIORITY + 5 )
+
 #define mainQUEUE_SEND_TASK1_PRIORITY       ( tskIDLE_PRIORITY + 1 )
 #define mainQUEUE_SEND_TASK2_PRIORITY       ( tskIDLE_PRIORITY + 2 )
-#define mainQUEUE_SEND_TASK3_PRIORITY       ( tskIDLE_PRIORITY + 2 )
-#define mainQUEUE_SEND_TASK4_PRIORITY       ( tskIDLE_PRIORITY + 2 )
+#define mainQUEUE_SEND_TASK3_PRIORITY       ( tskIDLE_PRIORITY + 3 )
+#define mainQUEUE_SEND_TASK4_PRIORITY       ( tskIDLE_PRIORITY + 4 )
 
 
 /* The rate at which data is sent to the queue.  The times are converted from
  * milliseconds to ticks using the pdMS_TO_TICKS() macro. */
-#define mainTASK1_SEND_FREQUENCY_MS         pdMS_TO_TICKS( 100UL )
-#define mainTASK2_SEND_FREQUENCY_MS         pdMS_TO_TICKS( 300UL )
-#define mainTASK3_SEND_FREQUENCY_MS         pdMS_TO_TICKS( 500UL )
-#define mainTASK4_SEND_FREQUENCY_MS         pdMS_TO_TICKS( 700UL )
-#define mainTIMER_SEND_FREQUENCY_MS         pdMS_TO_TICKS( 2000UL )
+#define mainTASK1_SEND_FREQUENCY_MS         pdMS_TO_TICKS( 18UL )
+#define mainTASK2_SEND_FREQUENCY_MS         pdMS_TO_TICKS( 60UL )
+#define mainTASK3_SEND_FREQUENCY_MS         pdMS_TO_TICKS( 36UL )
+#define mainTASK4_SEND_FREQUENCY_MS         pdMS_TO_TICKS( 12UL )
+#define mainTIMER_SEND_FREQUENCY_MS         pdMS_TO_TICKS( 18000UL )
 
 /* The number of items the queue can hold at once. */
 #define mainQUEUE_LENGTH                   ( 5 )
@@ -126,6 +127,7 @@
  * The tasks as described in the comments at the top of this file.
  */
 static void prvQueueReceiveTask( void * pvParameters );
+
 static void prvQueueSendTask1( void * pvParameters );
 static void prvQueueSendTask2( void * pvParameters );
 static void prvQueueSendTask3( void * pvParameters );
@@ -153,33 +155,21 @@ void ipsa_sched( void )
 
     if( xQueue != NULL )
     {
-        /* Start the two tasks as described in the comments at the top of this
-         * file. */
+        /* Start the two tasks as described in the comments at the top of this file. */
         xTaskCreate( prvQueueReceiveTask,"Rx", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_RECEIVE_TASK_PRIORITY, NULL );
         xTaskCreate( prvQueueSendTask1, "T1X", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_SEND_TASK1_PRIORITY, NULL );
         xTaskCreate( prvQueueSendTask2, "T2X", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_SEND_TASK2_PRIORITY, NULL );
         xTaskCreate( prvQueueSendTask3, "T3X", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_SEND_TASK3_PRIORITY, NULL );
         xTaskCreate( prvQueueSendTask4, "T4X", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_SEND_TASK4_PRIORITY, NULL );
-    /* Create the software timer, but don't start it yet. */
-        xTimer = xTimerCreate( "Timer",                     /* The text name assigned to the software timer - for debug only as it is not used by the kernel. */
-                               xTimerPeriod,                /* The period of the software timer in ticks. */
-                               pdTRUE,                      /* xAutoReload is set to pdTRUE. */
-                               NULL,                        /* The timer's ID is not used. */
-                               prvQueueSendTimerCallback ); /* The function executed when the timer expires. */
+        /* Create the software timer, but don't start it yet. */
+        xTimer = xTimerCreate( "Timer", xTimerPeriod, pdTRUE, NULL, prvQueueSendTimerCallback ); 
 
         if( xTimer != NULL )
         {
             xTimerStart( xTimer, 0 );
         }
-
-        /* Start the tasks and timer running. */
         vTaskStartScheduler();
     }
-    /* If all is well, the scheduler will now be running, and the following
-     * line will never be reached.  If the following line does execute, then
-     * there was insufficient FreeRTOS heap memory available for the idle and/or
-     * timer tasks	to be created.  See the memory management section on the
-     * FreeRTOS web site for more details. */
     for( ; ; )
     {
     }
@@ -190,46 +180,22 @@ void ipsa_sched( void )
 static void prvQueueSendTimerCallback( TimerHandle_t xTimerHandle )
 {
     const uint32_t ulValueToSend = mainVALUE_SENT_FROM_TIMER;
-
-    /* This is the software timer callback function.  The software timer has a
-     * period of two seconds and is reset each time a key is pressed.  This
-     * callback function will execute if the timer expires, which will only happen
-     * if a key is not pressed for two seconds. */
-
-    /* Avoid compiler warnings resulting from the unused parameter. */
     ( void ) xTimerHandle;
-
-    /* Send to the queue - causing the queue receive task to unblock and
-     * write out a message.  This function is called from the timer/daemon task, so
-     * must not block.  Hence the block time is set to 0. */
     xQueueSend( xQueue, &ulValueToSend, 0U );
 }
 /*-----Fonction pour chaque tache------------------------------------------*/
+
 /*TACHE1*/
 static void prvQueueSendTask1( void * pvParameters )
 {
     TickType_t xNextWakeTime;
     const TickType_t xBlockTime = mainTASK1_SEND_FREQUENCY_MS;
     const uint32_t ulValueToSend = mainVALUE_SENT_FROM_TASK1;
-
-    /* Prevent the compiler warning about the unused parameter. */
     ( void ) pvParameters;
-
-    /* Initialise xNextWakeTime - this only needs to be done once. */
     xNextWakeTime = xTaskGetTickCount();
-
     for( ; ; )
     {
-        /* Place this task in the blocked state until it is time to run again.
-        *  The block time is specified in ticks, pdMS_TO_TICKS() was used to
-        *  convert a time specified in milliseconds into a time specified in ticks.
-        *  While in the Blocked state this task will not consume any CPU time. */
         vTaskDelayUntil( &xNextWakeTime, xBlockTime );
-
-        /* Send to the queue - causing the queue receive task to unblock and
-         * write to the console.  0 is used as the block time so the send operation
-         * will not block - it shouldn't need to block as the queue should always
-         * have at least one space at this point in the code. */
         xQueueSend( xQueue, &ulValueToSend, 0U );
     }
 }
@@ -239,81 +205,42 @@ static void prvQueueSendTask2( void * pvParameters )
     TickType_t xNextWakeTime;
     const TickType_t xBlockTime = mainTASK2_SEND_FREQUENCY_MS;
     const uint32_t ulValueToSend = mainVALUE_SENT_FROM_TASK2;
-
-    /* Prevent the compiler warning about the unused parameter. */
     ( void ) pvParameters;
-
-    /* Initialise xNextWakeTime - this only needs to be done once. */
     xNextWakeTime = xTaskGetTickCount();
-
     for( ; ; )
     {
-        /* Place this task in the blocked state until it is time to run again.
-        *  The block time is specified in ticks, pdMS_TO_TICKS() was used to
-        *  convert a time specified in milliseconds into a time specified in ticks.
-        *  While in the Blocked state this task will not consume any CPU time. */
         vTaskDelayUntil( &xNextWakeTime, xBlockTime );
-
-        /* Send to the queue - causing the queue receive task to unblock and
-         * write to the console.  0 is used as the block time so the send operation
-         * will not block - it shouldn't need to block as the queue should always
-         * have at least one space at this point in the code. */
         xQueueSend( xQueue, &ulValueToSend, 0U );
     }
 }
+
 /*TACHE3*/
 static void prvQueueSendTask3( void * pvParameters )
 {
     TickType_t xNextWakeTime;
     const TickType_t xBlockTime = mainTASK3_SEND_FREQUENCY_MS;
     const uint32_t ulValueToSend = mainVALUE_SENT_FROM_TASK3;
-
-    /* Prevent the compiler warning about the unused parameter. */
     ( void ) pvParameters;
-
-    /* Initialise xNextWakeTime - this only needs to be done once. */
     xNextWakeTime = xTaskGetTickCount();
 
     for( ; ; )
     {
-        /* Place this task in the blocked state until it is time to run again.
-        *  The block time is specified in ticks, pdMS_TO_TICKS() was used to
-        *  convert a time specified in milliseconds into a time specified in ticks.
-        *  While in the Blocked state this task will not consume any CPU time. */
         vTaskDelayUntil( &xNextWakeTime, xBlockTime );
-
-        /* Send to the queue - causing the queue receive task to unblock and
-         * write to the console.  0 is used as the block time so the send operation
-         * will not block - it shouldn't need to block as the queue should always
-         * have at least one space at this point in the code. */
         xQueueSend( xQueue, &ulValueToSend, 0U );
     }
 }
+
 /*TACHE4*/
 static void prvQueueSendTask4( void * pvParameters )
 {
     TickType_t xNextWakeTime;
     const TickType_t xBlockTime = mainTASK4_SEND_FREQUENCY_MS;
     const uint32_t ulValueToSend = mainVALUE_SENT_FROM_TASK4;
-
-    /* Prevent the compiler warning about the unused parameter. */
     ( void ) pvParameters;
-
-    /* Initialise xNextWakeTime - this only needs to be done once. */
     xNextWakeTime = xTaskGetTickCount();
-
     for( ; ; )
     {
-        /* Place this task in the blocked state until it is time to run again.
-        *  The block time is specified in ticks, pdMS_TO_TICKS() was used to
-        *  convert a time specified in milliseconds into a time specified in ticks.
-        *  While in the Blocked state this task will not consume any CPU time. */
         vTaskDelayUntil( &xNextWakeTime, xBlockTime );
-
-        /* Send to the queue - causing the queue receive task to unblock and
-         * write to the console.  0 is used as the block time so the send operation
-         * will not block - it shouldn't need to block as the queue should always
-         * have at least one space at this point in the code. */
         xQueueSend( xQueue, &ulValueToSend, 0U );
     }
 }
@@ -323,36 +250,23 @@ static void prvQueueSendTask4( void * pvParameters )
 static void prvQueueReceiveTask( void * pvParameters )
 {
     uint32_t ulReceivedValue;
-
-    /* Prevent the compiler warning about the unused parameter. */
     ( void ) pvParameters;
-
     for( ; ; )
-    {
-        /* Wait until something arrives in the queue - this task will block
-         * indefinitely provided INCLUDE_vTaskSuspend is set to 1 in
-         * FreeRTOSConfig.h.  It will not use any CPU time while it is in the
+    { in the
          * Blocked state. */
         xQueueReceive( xQueue, &ulReceivedValue, portMAX_DELAY );
-
-        /* To get here something must have been received from the queue, but
-         * is it an expected value?  Normally calling printf() from a task is not
-         * a good idea.  Here there is lots of stack space and only one task is
-         * using console IO so it is ok.  However, note the comments at the top of
-         * this file about the risks of making Linux system calls (such as
-         * console output) from a FreeRTOS task. */
-        if( ulReceivedValue == mainVALUE_SENT_FROM_TASK1)
+        if( ulReceivedValue == mainVALUE_SENT_FROM_TASK1)  //Tache 1 executée si val1 recu dans la file d'attente
         {
             console_print( "Working\n" );
         }
-        else if( ulReceivedValue == mainVALUE_SENT_FROM_TASK2)
+        else if( ulReceivedValue == mainVALUE_SENT_FROM_TASK2)//Tache 2 executée si val2 recu dans la file d'attente
         {
             double celsius = 15.0;
             double fahrenheit;
             fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
             console_print("%.2f Fahrenheit\n", fahrenheit);
     
-        }else if( ulReceivedValue == mainVALUE_SENT_FROM_TASK3)
+        }else if( ulReceivedValue == mainVALUE_SENT_FROM_TASK3)//Tache 3 executée si val3 recu dans la file d'attente
         {   
             long int num1 = 1234567890;
             long int num2 = 9876543210;
@@ -360,7 +274,7 @@ static void prvQueueReceiveTask( void * pvParameters )
             result = num1 * num2;
             console_print("Result of the multiplication of is : %ld \n",result);
     
-        }else if( ulReceivedValue == mainVALUE_SENT_FROM_TASK4)
+        }else if( ulReceivedValue == mainVALUE_SENT_FROM_TASK4)//Tache 4 executée si val4 recu dans la file d'attente
         {
             
             // Crée une liste de 50 éléments (nombres de 1 à 50)
@@ -390,7 +304,7 @@ static void prvQueueReceiveTask( void * pvParameters )
 
         }else if( ulReceivedValue == mainVALUE_SENT_FROM_TIMER)
         {
-            console_print( "Message received from software timer\n" );
+            console_print( "HyperPeriod reached \n" );
         }
         else 
         {
